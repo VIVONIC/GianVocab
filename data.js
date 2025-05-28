90e08f108681308abc613d3cb42a4f9082398bc9
VIVONIC <cloverskywintermango@gmail.com>
2025-05-27 22:26:22 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,23 @@
+const data =
+[
+    {
+        id: 1,
+        phrase: 'Ion een curr fr',
+        meaning: 'I dont even care for real',
+        example: 'Wow I got no sets, Ion een curr fr',
+        
+    },
+
+    {
+        id: 2,
+        phrase: 'Auuugh',
+        meaning: 'Depends on the context, sometimes its just to make a sound but sometimes it is a neutral reaction or sign of lazyness',
+        example: 'I dont wanna go to school auuuugh',
+        
+    }
+
+    
+
+]
+
+export default data
